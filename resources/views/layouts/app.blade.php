a173831d196fff26327994063f92f9187284d1c5
Pragalbha Patil <pragalbha.p@somaiya.edu>
2019-11-04 17:56:25 +0530

    added preloader

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> -->
     <script src="https://use.fontawesome.com/0752da24be.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
@@ -27,6 +28,38 @@
     
 </head>
 <body>
+    <!-- Preloader style, script and div -->
+    <style>
+    .no-js #loader { display: none;  }
+    .js #loader { display: block; position: absolute; left: 100px; top: 0; }
+    .se-pre-con {
+        position: fixed;
+        left: 0px;
+        top: 0px;
+        width: 100%;
+        height: 100%;
+        z-index: 9999;
+        background: url("/preloader_trans.gif") center no-repeat #fff;
+    }    
+    </style>
+    <script>
+    // Jquery 3.14 works with below ONLY
+    $(window).on('load', function () {
+        // Animate loader off screen
+        console.log('ready');
+		$(".se-pre-con").fadeOut("slow");
+    });
+    // old jquery works with below so removed.
+
+    // $(window).load(function() {
+	// 	// Animate loader off screen
+    //     console.log('ready');
+	// 	$(".se-pre-con").fadeOut("slow");
+    //     $(".se-pre-con").hide();
+	// });
+    
+    </script>
+    <div class="se-pre-con"></div>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
